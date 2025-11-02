b3c2c07057f460e4adc894fde3271f13f7b4d0a7
David McAllister <davemc0@gmail.com>
2025-11-02 00:38:22 -0600

    Refactor and add businesses

--- bankcsv.py
+++ bankcsv.py
@@ -13,102 +13,136 @@ fieldnames = ['Account', 'Date', 'Description', 'cat', 'det', 'Amount']
 substitutions = [
 # {'str': 'ADVANCED MICRO D', 'cat': 'Business Travel', 'det': 'reim'},
 # {'str': 'AMD INC.', 'cat': 'Salary'},
-{'str': "MACEY'S EXPRESS HLD", 'cat': 'Automobile'},
+{'str': "ARBY'S", 'cat': 'Dining'},
+{'str': "CLAIRE'S BTQ", 'cat': 'Personal Care'},
+{'str': "MACEY'S EXPR", 'cat': 'Automobile'},
 {'str': "MACEY'S HOLL", 'cat': 'Groceries'},
-{'str': 'SPROUTS', 'cat': 'Groceries'},
 {'str': "MCDONALD'S", 'cat': 'Dining'},
+{'str': "MENCHIE'S", 'cat': 'Dining'},
+{'str': "MIKE'S MARKET", 'cat': 'Vacation'},
+{'str': "O'REILLY AUTO PARTS", 'cat': 'Automobile'},
 {'str': "SCHWAN'S HOME SERVIC", 'cat': 'Groceries', 'det': 'Schwans'},
-{'str': 'ASPEN RIDGE MAN', 'cat': 'Education', 'det': 'Jonathan Rent'},
+{'str': "STUDIOL", 'cat': 'Personal Care'},
 {'str': "WENDY'S", 'cat': 'Dining'},
 {'str': '7-ELEVEN', 'cat': 'Automobile', 'min': -25},
 {'str': '7-ELEVEN', 'cat': 'Dining', 'max': -25},
-{'str': 'MAVERIK', 'cat': 'Automobile', 'min': -25},
-{'str': 'MAVERIK', 'cat': 'Dining', 'max': -25},
 {'str': 'ACE HARDWARE', 'cat': 'Repairs'},
 {'str': 'ACTIVESKIN', 'cat': 'Medical'},
 {'str': 'AFGHAN KITCHEN', 'cat': 'Dining'},
-{'str': 'MY PIE P', 'cat': 'Dining'},
 {'str': 'AMAZON MARKEPLACE', 'cat': 'Housewares', 'det': 'Amazon', 'max': -250.0},
 {'str': 'AMAZON.COM', 'cat': 'Housewares', 'det': 'Amazon', 'max': -250.0},
 {'str': 'AMAZON.COM*', 'cat': 'Housewares', 'det': 'Amazon', 'max': -250.0},
 {'str': 'AMERICAN EXPRESS TYPE: ONLINE PMT', 'cat': '$ Pay AmEx'},
 {'str': 'AMEX EPAYMENT TYPE: ONLINE PMT', 'cat': '$ Pay AmEx'},
-{'str': 'APPLE.COM', 'cat': 'Entertainment'},
-{'str': 'AUTO PARTS', 'cat': 'Automobile'},
 {'str': 'AMEX RIDESHARE CREDIT', 'cat': 'Automobile'},
+{'str': 'AMZN DIGITAL', 'cat': 'Entertainment', 'det': 'Amazon', 'max': -25.0},
+{'str': 'APPLE.COM', 'cat': 'Entertainment'},
+{'str': 'ARCTIC CIRCLE', 'cat': 'Dining'},
+{'str': 'ASPEN RIDGE MAN', 'cat': 'Education', 'det': 'Jonathan Rent'},
+{'str': 'AUTO PARTS', 'cat': 'Automobile'},
 {'str': 'AUTOPAY PAYMENT', 'cat': '$ Pay AmEx'},
 {'str': 'BALANCEDBODY', 'cat': 'Medical'},
-{'str': 'UTAHTAXRFD', 'cat': 'Taxes'},
+{'str': 'BEAR TRAPPER', 'cat': 'Vacation'},
+{'str': 'BEAUMONT BAKERY', 'cat': 'Dining'},
+{'str': 'BENEVITY', 'cat': 'Donation'},
+{'str': 'BOONDOCKS', 'cat': 'Entertainment'},
+{'str': 'BURGER', 'cat': 'Dining'},
 {'str': 'BYU', 'cat': 'Education'},
-{'str': 'CAFE RIO', 'cat': 'Dining'},
-{'str': 'HONEST E', 'cat': 'Dining'},
-{'str': 'COSTA V', 'cat': 'Dining'},
-{'str': 'ARCTIC CIRCLE', 'cat': 'Dining'},
+{'str': 'CAFE', 'cat': 'Dining'},
 {'str': 'CAROLLYNN', 'cat': 'Education', 'det': 'Piano'},
 {'str': 'CARRINGTON', 'cat': 'Mortgage'},
+{'str': 'CARWA', 'cat': 'Automobile'},
+{'str': 'CENTRACOM', 'cat': 'Utilities', 'det': 'Internet'},
+{'str': 'CENTRAL LOAN ADMIN', 'cat': 'Mortgage'},
 {'str': 'CHEVRON', 'cat': 'Automobile'},
-{'str': "O'REILLY AUTO PARTS", 'cat': 'Automobile'},
+{'str': 'CHICK-FIL-A', 'cat': 'Dining'},
 {'str': 'CHUCKS SERVICE', 'cat': 'Automobile'},
+{'str': 'COSTA V', 'cat': 'Dining'},
 {'str': 'COSTA VIDA', 'cat': 'Dining'},
 {'str': 'COSTCO BY INSTACART', 'cat': 'Groceries'},
 {'str': 'COSTCO WHSE', 'cat': 'Groceries', 'max': -250.0},
 {'str': 'COTTONWOOD ID', 'cat': 'Utilities', 'det': 'Sewer'},
 {'str': 'DAIRY QUEEN', 'cat': 'Dining'},
+{'str': 'DAN S ACE', 'cat': 'Repairs'},
+{'str': 'DESERET BOOK', 'cat': 'Housewares'},
+{'str': 'DINER', 'cat': 'Dining'},
 {'str': 'DOLLAR TREE', 'cat': 'Housewares'},
-{'str': 'ENBRIDGE', 'cat': 'Utilities', 'det': 'Gas'},
+{'str': 'DOMINION ENERGY', 'cat': 'Utilities', 'det': 'Gas'},
 {'str': 'DOORDASH', 'cat': 'Dining'},
 {'str': 'DOWNEAST', 'cat': 'Housewares'},
 {'str': 'DTV*DIRECTV SERVICE', 'cat': 'Utilities', 'det': 'DirecTV'},
+{'str': 'Draft 3', 'cat': 'Housewares', 'det': 'Nora Jimenez Cleaning', 'max': -276.0, 'min': -249.0},
 {'str': 'ELECTRONIC PAYMENT RECEIVED', 'cat': '$ Pay AmEx'},
+{'str': 'ENBRIDGE', 'cat': 'Utilities', 'det': 'Gas'},
 {'str': 'EXXONMOBIL', 'cat': 'Automobile'},
-{'str': 'HERTZ CAR RENTAL', 'cat': 'Business Travel'},
 {'str': 'FANDANGO', 'cat': 'Entertainment'},
+{'str': 'FIVE BELOW', 'cat': 'Housewares'},
+{'str': 'FONS-SARAHJANEWATTS', 'cat': 'Education', 'det': 'Caroline voice'},
+{'str': 'FROSTS BOOKS', 'cat': 'Housewares'},
+{'str': 'Fort Union_104 Cottonwood Heights', 'cat': 'Dining', 'det': 'CAFE ZUPAS'},
 {'str': 'From DLT', 'cat': 'Capital Xfer'},
 {'str': 'From MCALLISTER', 'cat': 'Capital Xfer'},
-{'str': 'IRS T', 'cat': 'Taxes'},
 {'str': 'GEICO AUTO', 'cat': 'Automobile'},
 {'str': 'GOOGLE *FIBER', 'cat': 'Utilities', 'det': 'Internet'},
-{'str': 'HOLLIDAY WATER CO', 'cat': 'Utilities', 'det': 'Water'},
 {'str': 'GOOGLE*FIBER', 'cat': 'Utilities', 'det': 'Internet'},
-{'str': "STUDIOL", 'cat': 'Personal Care'},
-{'str': "CLAIRE'S BTQ", 'cat': 'Personal Care'},
-{'str': 'GREAT CLIPS', 'cat': 'Personal Care'},
+{'str': 'GREAT CL', 'cat': 'Personal Care'},
 {'str': 'GREAT HARVEST', 'cat': 'Groceries'},
+{'str': 'GRILL', 'cat': 'Dining'},
 {'str': 'HALE CENTRE', 'cat': 'Entertainment'},
+{'str': 'HALECENTRE', 'cat': 'Entertainment'},
 {'str': 'HARMONS', 'cat': 'Groceries'},
+{'str': 'HERTZ CAR RENTAL', 'cat': 'Business Travel'},
+{'str': 'HOBBY LOBBY', 'cat': 'Housewares'},
 {'str': 'HOLIDAY OIL', 'cat': 'Automobile'},
+{'str': 'HOLLIDAY WATER CO', 'cat': 'Utilities', 'det': 'Water'},
+{'str': 'HONEST E', 'cat': 'Dining'},
 {'str': 'IHOP', 'cat': 'Dining'},
 {'str': 'INTERMOUNTAIN HEALTH', 'cat': 'Medical'},
+{'str': 'IRS T', 'cat': 'Taxes'},
+{'str': 'Interest Charge on Purchases', 'cat': 'Interest'},
 {'str': 'JIFFY LUBE', 'cat': 'Automobile'},
+{'str': 'JIMMY JOHNS', 'cat': 'Dining'},
+{'str': 'JOANN STORES', 'cat': 'Housewares'},
 {'str': 'JOANNE', 'cat': 'Housewares'},
+{'str': 'JOHNNY ROCKETS', 'cat': 'Dining'},
 {'str': 'JUST.INGREDIENTS', 'cat': 'Housewares'},
 {'str': 'KFC', 'cat': 'Dining'},
 {'str': 'KINDLE SVCS', 'cat': 'Entertainment'},
 {'str': 'KOHLS', 'cat': 'Housewares'},
 {'str': 'LABCORP', 'cat': 'Medical'},
+{'str': 'LAWNGEVITY', 'cat': 'Yard'},
+{'str': 'LAYLA MED', 'cat': 'Dining'},
 {'str': 'LEDINGHAM PROPER', 'cat': 'Rental Income'},
 {'str': 'LES SCHWAB', 'cat': 'Automobile'},
 {'str': 'LILLIAN FARRIS', 'cat': 'Medical', 'det': 'Hazel'},
 {'str': 'LITTLE CAESAR', 'cat': 'Dining'},
-{'str': 'TOP IT FROZEN', 'cat': 'Dining'},
-{'str': 'SUBWAY', 'cat': 'Dining'},
 {'str': 'LOANCARE', 'cat': 'Mortgage'},
 {'str': 'LUME DEODORANT', 'cat': 'Housewares'},
-{'str': 'JOANN STORES', 'cat': 'Housewares'},
 {'str': 'MARSHALLS', 'cat': 'Housewares'},
+{'str': 'MAVERIK', 'cat': 'Automobile', 'min': -25},
+{'str': 'MAVERIK', 'cat': 'Dining', 'max': -25},
 {'str': 'MEIERS PHARMACY', 'cat': 'Medical'},
+{'str': 'MICHAELS STOR', 'cat': 'Housewares'},
 {'str': 'MILLCREEK GARDENS', 'cat': 'Yard'},
+{'str': 'ML PHY THERAPY', 'cat': 'Medical'},
+{'str': 'MOBILE PAYMENT', 'cat': '$ Pay AmEx'},
 {'str': 'MORTGAGE', 'cat': 'Mortgage'},
 {'str': 'MT OLYMPUS IMPROVE', 'cat': 'Utilities', 'det': 'Sewer'},
+{'str': 'MY PIE P', 'cat': 'Dining'},
+{'str': 'MY529', 'cat': '$ UESP'},
 {'str': 'NAYAX WASH', 'cat': 'Automobile'},
-{'str': 'PARKING', 'cat': 'Automobile'},
 {'str': 'NETFLIX', 'cat': 'Entertainment'},
+{'str': 'NEWREZ', 'cat': 'Mortgage'},
+{'str': 'NSTS MANAGEMENT', 'cat': 'Rental Income Net'},
+{'str': 'Nintendo', 'cat': 'Entertainment'},
 {'str': 'ODP Fee', 'cat': 'Bank Fees'},
 {'str': 'OHSWEBSTOR', 'cat': 'Education'},
-{'str': 'FONS-SARAHJANEWATTS', 'cat': 'Education', 'det': 'Caroline voice'},
 {'str': 'OLYMPUS CLINIC', 'cat': 'Medical'},
 {'str': 'OLYMPUS FAMILY MED', 'cat': 'Medical'},
+{'str': 'ONLINE PAYMENT', 'cat': '$ Pay AmEx'},
 {'str': 'PACIFICORP', 'cat': 'Utilities', 'det': 'Power'},
+{'str': 'PANDA EXPR', 'cat': 'Dining'},
+{'str': 'PARKING', 'cat': 'Automobile'},
 {'str': 'PEDIATRIC', 'cat': 'Medical'},
 {'str': 'PENN MUTUAL', 'cat': '$ Life Ins', 'det': 'Penn Mutual'},
 {'str': 'PIZZA', 'cat': 'Dining'},
@@ -116,9 +150,12 @@ substitutions = [
 {'str': 'PRELOVED', 'cat': 'Housewares'},
 {'str': 'PRIMARY CHILDREN', 'cat': 'Medical'},
 {'str': 'PRIME VIDEO', 'cat': 'Entertainment'},
+{'str': 'QUANTUM', 'cat': 'Utilities', 'det': 'Internet'},
 {'str': 'QUESTAR GAS', 'cat': 'Utilities', 'det': 'Gas'},
 {'str': 'RANCHERITO', 'cat': 'Dining'},
 {'str': 'RED 8 ASIAN', 'cat': 'Dining'},
+{'str': 'RELIASTAR', 'cat': '$ Life Ins', 'det': 'Voya'},
+{'str': 'RESTAURANT', 'cat': 'Dining'},
 {'str': 'ROSS DRESS FOR LESS', 'cat': 'Housewares'},
 {'str': 'ROSS STORES', 'cat': 'Housewares'},
 {'str': 'SALTLAKECOUNTYLIBRARYS', 'cat': 'Entertainment'},
@@ -131,11 +168,13 @@ substitutions = [
 {'str': 'SNAPFISH', 'cat': 'Housewares'},
 {'str': 'SPECIALIZED LOAN', 'cat': 'Mortgage'},
 {'str': 'SPEEDWAY', 'cat': 'Automobile'},
+{'str': 'SPROUTS', 'cat': 'Groceries'},
 {'str': 'STEAM GAMES', 'cat': 'Entertainment'},
 {'str': 'SUBARU', 'cat': 'Automobile'},
+{'str': 'SUBWAY', 'cat': 'Dining'},
 {'str': 'SUMMIT FINANCIAL', 'cat': 'Legal and Prof Fees', 'det': 'Tax prep'},
+{'str': 'SUSHI', 'cat': 'Dining'},
 {'str': 'SWEETALY', 'cat': 'Dining'},
-{'str': 'TROPICAL SMOOTHIE CA', 'cat': 'Dining'},
 {'str': 'SWINYER WOSETH', 'cat': 'Medical'},
 {'str': 'SWITCH SALON', 'cat': 'Personal Care'},
 {'str': 'T J MAXX', 'cat': 'Housewares'},
@@ -146,6 +185,10 @@ substitutions = [
 {'str': 'TICKETMAST', 'cat': 'Entertainment'},
 {'str': 'TJ MAXX', 'cat': 'Housewares'},
 {'str': 'TMOBILE', 'cat': 'Utilities', 'det': 'Cell Phone'},
+{'str': 'TOP IT FROZEN', 'cat': 'Dining'},
+{'str': 'TRADER JOE', 'cat': 'Groceries'},
+{'str': 'TROPICAL SMOO', 'cat': 'Dining'},
+{'str': 'TROPICAL SMOOTHIE CA', 'cat': 'Dining'},
 {'str': 'Transfer From Loan', 'cat': '$ Loan Xfer'},
 {'str': 'Transfer To Loan 02', 'cat': 'Automobile', 'det': 'Legacy'},
 {'str': 'Transfer To Loan 03', 'cat': 'Automobile', 'det': 'Santa Fe'},
@@ -159,6 +202,7 @@ substitutions = [
 {'str': 'USPS', 'cat': 'Housewares'},
 {'str': 'UTAH CORPORATIONS', 'cat': 'Legal and Prof Fees'},
 {'str': 'UTAH-DMV', 'cat': 'Automobile'},
+{'str': 'UTAHTAXRFD', 'cat': 'Taxes'},
 {'str': 'VALLEY WIDE COOP', 'cat': 'Utilities', 'det': 'Propane'},
 {'str': 'VIDANGEL', 'cat': 'Entertainment'},
 {'str': 'VILLAGE TOWNHOME', 'cat': 'HOA Dues'},
@@ -172,6 +216,17 @@ substitutions = [
 {'str': 'WYZE LABS', 'cat': 'Legal and Prof Fees'},
 {'str': 'ZEST FOR LIFE', 'cat': 'Medical'},
 {'str': 'ZIONS BANK TYPE: ONLINE PMT', 'cat': 'Zions Interest'},
+]
+
+replacements = [
+    ('Canyon View RDC', 'Canyon View Mobile Check Deposit'),
+    ('THE HOME DEPOT', 'HOME DEPOT'),
+    ('The Home Depot', 'HOME DEPOT'),
+    ('THE HOME DEPO', 'HOME DEPOT '),
+    ('MEMO:', ' MEMO:'),
+    ('CO: Urban FT', ''),
+    ('AplPay ', ''),
+    ('1112 DOWNEAST', 'DOWNEAST')
 ]
 
 def categorize(row):
@@ -189,13 +244,6 @@ def categorize(row):
             if 'det' in item:
                 row['det'] = item['det']
             return row
-
-    # Special cases
-    if 'Draft 3' in desc and float(row['Amount']) == -250.0:
-        row['cat'] = 'Housewares'
-        row['det'] = 'Nora Jimenez Cleaning'
-    if 'Mobile Deposit' in desc and 'rent' in desc:
-        row['cat'] = 'Rental Income Net'
 
     return row
 
@@ -222,7 +270,7 @@ def removePrefixes(content):
     '''Remove prefixes separated by punctuation from descriptions'''
 
     before = content
-    content = removeOnePrefix(content, '*', ['ACT', 'AMZ', 'BT', 'EB', 'ETT', 'FS', 'GG', 'ICP', 'INT', 'PAYPAL', 'POS', 'PTI', 'PY', 'RAL', 'SP', 'SQ', 'TM', 'TST', 'WPY', 'YSI'])
+    content = removeOnePrefix(content, '*', ['ACT', 'AMZ', 'BT', 'EB', 'ETT', 'FS', 'GG', 'ICP', 'IN', 'INT', 'PAYPAL', 'POS', 'PTI', 'PY', 'RAL', 'SP', 'SQ', 'TM', 'TST', 'WPY', 'YSI', 'Debit Card APF'])
     content = removeOnePrefix(content, ' ', ['WWW', 'SP'])
     content = removeOnePrefix(content, '.', ['WWW'])
 
@@ -231,10 +279,9 @@ def removePrefixes(content):
 
     return content
 
-def process_ufirstcu_file(file_path):
-    '''AccountHistory.csv is from UFirstCU'''
-
-    # Clean up line ends, etc.
+def preprocess(file_path, outfile):
+    '''Clean up line ends, etc.'''
+    
     with open(file_path, 'rb') as open_file:
         content = open_file.read()
 
@@ -257,6 +304,50 @@ def process_ufirstcu_file(file_path):
     with open('tmp.csv', 'wb') as open_file:
         open_file.write(content)
 
+def cleanup_description(desc):
+    '''Clean up description'''
+    
+    for chop in ['Withdrawal by', 'Deposit by', 'Withdrawal', 'Deposit', 'BUSINESS DEBIT', 'Visa Debit', 'Bill Payment', 'Bill Paid-', 'Debit Card']:
+        if desc != 'Withdrawal' and desc != 'Deposit' and 'Withdrawal at ATM' not in desc and 'Debit Card Fee' not in desc and 'Mobile Deposit' not in desc:
+            desc = desc.replace(chop, '')
+
+    for bad, good in replacements:
+        desc = desc.replace(bad, good)
+        
+    desc = removePrefixes(desc)
+
+    if 'in the amount' in desc:
+        desc = 'Fee Withdrawal Overdrawn ' + desc
+
+    if '/ Transfer' in desc:
+        desc = desc.split('/ ')[1] + ' ' + desc.split('/ ')[0]
+
+    if 'TYPE:' in desc and 'CO:' in desc:
+        company = desc.split('CO: ')[1]
+        company = company.replace('Entry Class Code', 'CODE')
+        company = company.split(':')[0] + ':'
+        company = company.replace(' NAME:', '')
+        company = company.replace(' CODE:', '')
+        company = company.replace(' DATA:', '')
+        company = company.replace(':', '')
+        desc = company + ' ' + desc
+
+    # Eat initial digits
+    if '7-11' not in desc and '7-ELEVEN' not in desc:
+        while len(desc) and desc[0] in list('#0123456789 '):
+            desc = desc[1:]
+    
+    desc = replaceLoop(desc, '\r', ' ', False, '\n') # Remove internal line break
+    desc = replaceLoop(desc, '\n', ' ', False, '\n') # Remove internal line break        
+    desc = replaceLoop(desc, '  ', ' ', False, '\n')
+
+    return desc
+
+def process_canyonviewcu_file(file_path):
+    '''AccountHistory.csv is from Canyon View CU'''
+
+    preprocess(file_path, 'tmp.csv')
+    
     # Process file, row by row, clean up columns, etc.
     rows = []
 
@@ -277,46 +368,11 @@ def process_ufirstcu_file(file_path):
             row['det'] = row['Check']
             row.pop('Check')
             row.pop('Status')
-            desc = row['Description']
+            
+            row['Description'] = cleanup_description(row['Description'])
 
-            for chop in ['Withdrawal by', 'Deposit by', 'Withdrawal', 'Deposit', 'BUSINESS DEBIT', 'Visa Debit', 'Bill Payment']:
-                if desc != 'Withdrawal' and desc != 'Deposit' and 'Withdrawal at ATM' not in desc and 'Mobile Deposit' not in desc:
-                    desc = desc.replace(chop, '')
-
-            desc = desc.replace('UFirst RDC', 'UFirst Mobile Check Deposit')
-            desc = desc.replace('THE HOME DEPOT', 'HOME DEPOT')
-            desc = desc.replace('The Home Depot', 'HOME DEPOT')
-            desc = desc.replace('MEMO:', ' MEMO:')
-            desc = desc.replace('CO: Urban FT', '')
-            desc = removePrefixes(desc)
-
-            if 'in the amount' in desc:
-                desc = 'Fee Withdrawal Overdrawn ' + desc
-
-            if '/ Transfer' in desc:
-                desc = desc.split('/ ')[1] + ' ' + desc.split('/ ')[0]
-
-            if 'MEMO:' in desc:
-                row['det'] = desc.split('MEMO: ')[1]
-
-            if 'TYPE:' in desc and 'CO:' in desc:
-                company = desc.split('CO: ')[1]
-                company = company.replace('Entry Class Code', 'CODE')
-                company = company.split(':')[0] + ':'
-                company = company.replace(' NAME:', '')
-                company = company.replace(' CODE:', '')
-                company = company.replace(' DATA:', '')
-                company = company.replace(':', '')
-                desc = company + ' ' + desc
-
-            # Eat initial digits
-            if '7-11' not in desc:
-                while len(desc) and desc[0] in list('#0123456789 '):
-                    desc = desc[1:]
-
-            desc = replaceLoop(desc, '  ', ' ', False, '\n')
-
-            row['Description'] = desc
+            if 'MEMO:' in row['Description']:
+                row['det'] = row['Description'].split('MEMO: ')[1]
 
             if row['Debit'] != '':
                 row['Amount'] = str(-float(row['Debit']))
@@ -334,7 +390,7 @@ def process_ufirstcu_file(file_path):
 
     os.remove('tmp.csv')
 
-    with open('ufirstcu.csv', 'w', newline='') as csvfile:
+    with open('canyonviewcu.csv', 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         writer.writeheader()
@@ -370,22 +426,9 @@ def process_amex_file(file_path):
             if 'Reference' in row:
                 row.pop('Reference')
 
-            # Clean up description
-            desc = row['Description']
-
-            desc = desc.replace('AplPay ', '')
-            desc = desc.replace('1112 DOWNEAST', 'DOWNEAST')
-            desc = desc.replace('THE HOME DEPOT', 'HOME DEPOT')
-
-            # Eat initial digits
-            if '7-ELEVEN' not in desc:
-                while len(desc) and desc[0] in list('#0123456789 '):
-                    desc = desc[1:]
-
-            desc = replaceLoop(desc, '  ', ' ', False, '\n')
-            desc = removePrefixes(desc)
-
-            row['Description'] = desc
+            row['Description'] = cleanup_description(row['Description'])
+            
+            # Amount
             row['Amount'] = str(-float(row['Amount'])) # Negate the amount for AmEx
 
             # Automatic categorization
@@ -404,8 +447,7 @@ def process_amex_file(file_path):
     print('Done with AmEx')
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='bankcsv.py - Fix up CSV files from banks')
+    parser = argparse.ArgumentParser(description='bankcsv.py - Fix up CSV files from banks')
 
     parser.add_argument('fname',
                         nargs='+',
@@ -415,10 +457,8 @@ def main():
 
     for fname in args.fname:
         print(fname)
-        if 'Transac' in fname:
-            process_uucu_file(fname)
-        elif 'Account' in fname:
-            process_ufirstcu_file(fname)
+        if 'Account' in fname:
+            process_canyonviewcu_file(fname)
         elif 'activity' in fname:
             process_amex_file(fname)
         else:
